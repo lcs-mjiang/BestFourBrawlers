414f868070c65b523a56f587c0b7ecccfb313960
Michael Jiang <mjiang@lcs.on.ca>
2024-11-20 10:49:03 -0500

    ended class

---
+++ BestFourBrawlers/Preview Content/BrawlerStructure.swift
@@ -0,0 +1,20 @@
+//
+//  BrawlerStructure.swift
+//  BestFourBrawlers
+//
+//  Created by Michael on 2024/11/20.
+//
+
+import Foundation
+
+struct Brawler {
+    let name: String
+    let rarity: String
+    let type: String
+    let description: String
+}
+
+let kit = Brawler(name: "Kit", rarity: "LEGENDARY", type: "SUPPORT", description: "Stuck giving autographs to his rabid fans at Starr Toon Studios, cartoony superstar KIt often daydreams of becoming relevant in the industry again, respected for his acting chops rather than his looks.")
+let kenji = Brawler(name: "Kenji", rarity: "LEGENDARY", type: "ASSASSIN", description: "This highly skilled sushi chef has a wholesome life running his restaurant in Starr Park, but it feels like he's trying to hide something about his past...though his sushi tastes so good, nobody cares!")
+let moe = Brawler(name: "Moe", rarity: "Mythic", type: "DAMAGE DEALER", description: "Moe is a blind rat, found living in the Starr Park sewers by Grom, who then quickly adopted him! Now part of Starr Park's maintenance team, Moe uses his special digging machine, Driller, to traverse the sewer tunnels...")
+let mico = Brawler(name: "Mico", rarity: "MYTHIC", type: "ASSASSIN", description: "Mico's the sound guy in Brawlywood, but one could be forgiven for thinking he's actually a celebrity given all the bragging and name-dropping. Hot-tempered and unstable, he cannot take a joke. Don't ever call him funny...")
---
+++ BestFourBrawlers/Preview Content/BrawlerView.swift
@@ -0,0 +1,23 @@
+//
+//  BrawlerView.swift
+//  BestFourBrawlers
+//
+//  Created by Michael on 2024/11/20.
+//
+
+import SwiftUI
+
+struct BrawlerView: View {
+    let providedBrawler: Brawler
+    var body: some View {
+        NavigationStack {
+            List {
+                BrawlerStructure(
+            }
+        }
+    }
+}
+
+#Preview {
+    BrawlerView()
+}
