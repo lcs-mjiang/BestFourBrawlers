fa587c158463dfe1a9abde8c5d4525ca2d6e21de
Michael Jiang <mjiang@lcs.on.ca>
2024-11-20 10:27:52 -0500

    Initial Commit

---
+++ BestFourBrawlers/BestFourBrawlersApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestFourBrawlersApp.swift
+//  BestFourBrawlers
+//
+//  Created by Michael on 2024/11/20.
+//
+
+import SwiftUI
+
+@main
+struct BestFourBrawlersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
